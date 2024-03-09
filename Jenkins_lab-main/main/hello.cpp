0c15caf8371cbc37e9d492d93d538c1d3db82944
vnk123 <100826065+vnk123@users.noreply.github.com>
2024-03-09 12:30:02 +0530

    Update hello.cpp

--- Jenkins_lab-main/main/hello.cpp
+++ Jenkins_lab-main/main/hello.cpp
@@ -1,4 +1,4 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "Webhook successfully enabled\n";  
   return 0;
 }
